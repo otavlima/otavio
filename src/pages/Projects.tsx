d3eefe4a12e7ab637a7586870922342f80687636
Otávio Lima <otavio-lima-co@gmail.com>
2026-03-16 20:14:51 -0300

    refector: communicate ai link

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,10 +9,10 @@ const Projects = () => {
         <div className="h-screen overflow-hidden">
           <div className="mt-64 max-[564px]:mt-32 max-w-96">
             <div className="mt-4 px-4">
-              <h2 className="text-[#D9D9D9] text-xl font-semibold">Crown Investments</h2>
-              <p className="text-sm text-[#D9D9D9] mt-2">Full-stack financial management system with a secure REST API (Spring Boot) and a fully responsive frontend (React + TypeScript + TailwindCSS).</p>
-              <a href='https://github.com/otavlima/crown-investments' target="_blank" rel="noopener noreferrer" className="flex items-center justify-between border border-[#404040] h-12 w-full px-6 rounded-xl mt-4 bg-[#0D0D0D] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
-                <span>Crown Investments</span>
+              <h2 className="text-[#D9D9D9] text-xl font-semibold">Communicate AI</h2>
+              <p className="text-sm text-[#D9D9D9] mt-2">Communicate AI — an AI assistant that helps you communicate better in professional and personal situations.</p>
+              <a href='https://communicate-9gtfd0oj4-otavio-lima-devs-projects.vercel.app/' target="_blank" rel="noopener noreferrer" className="flex items-center justify-between border border-[#404040] h-12 w-full px-6 rounded-xl mt-4 bg-[#0D0D0D] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
+                <span>Communicate AI</span>
                 <span><IoIosArrowForward /></span>
               </a>
             </div>
