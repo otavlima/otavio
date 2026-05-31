3e4403322f8bf28d82b322f4469c9e0957f2f460
Otávio Lima <otavio-lima-co@gmail.com>
2026-05-31 15:17:32 -0300

    chore: add fityness to projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -17,10 +17,10 @@ const Projects = () => {
               </a>
             </div>
             <div className="mt-4 px-4">
-              <h2 className="text-[#D9D9D9] text-xl font-semibold">Agents</h2>
-              <p className="text-sm text-[#D9D9D9] mt-2">Real-time Q&A app with audio recording and AI-powered search, built during Rocketseat’s NLW IA event.</p>
-              <a href='https://github.com/otavlima/agents' target="_blank" rel="noopener noreferrer" className="flex items-center justify-between border border-[#404040] h-12 w-full  px-6 rounded-xl mt-4 bg-[#0D0D0D] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
-                <span>Agents</span>
+              <h2 className="text-[#D9D9D9] text-xl font-semibold">Fity.ness</h2>
+              <p className="text-sm text-[#D9D9D9] mt-2">Fity.ness is a modern and intelligent fitness platform that helps users manage workouts, organize schedules, track progress and stay consistent through a clean experience powered by AI.</p>
+              <a href='https://about-fityness.vercel.app/' target="_blank" rel="noopener noreferrer" className="flex items-center justify-between border border-[#404040] h-12 w-full  px-6 rounded-xl mt-4 bg-[#0D0D0D] text-[#BFBDBA] text-xl hover:border-[#f2f2f2] hover:text-[#f2f2f2] transition duration-300 select-none shadow-[-2px_2px_4px_rgba(0,0,0,0.5)]">
+                <span>Fity.ness</span>
                 <span><IoIosArrowForward /></span>
               </a>
             </div>
